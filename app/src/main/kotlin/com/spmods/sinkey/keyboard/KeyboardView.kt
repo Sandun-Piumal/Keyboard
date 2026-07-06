90da4448674bdc9ed89bb769f3288ce3c3442db2
Sandun-Piumal <sandunpiyumal20@gmail.com>
2026-07-06 11:01:40 +0530

    Update KeyboardView.kt

--- app/src/main/kotlin/com/spmods/sinkey/keyboard/KeyboardView.kt
+++ app/src/main/kotlin/com/spmods/sinkey/keyboard/KeyboardView.kt
@@ -770,7 +770,7 @@ private val SymbolsRow2 = listOf("@","#","₹","%","&","*","-","=","(",")")
 private val SymbolsRow3 = listOf("!","\"","'",":","+","/","?")
 
 @Composable
-fun SymbolsKeyboardView(
+internal fun SymbolsKeyboardView(
     colors: KeyboardColors,
     keyHeight: Dp,
     keyShape: RoundedCornerShape,
