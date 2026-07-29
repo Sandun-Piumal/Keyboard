e2cffb3c24d7edfd77e0609ce0323c6b4bbacb5a
Sandun-Piumal <sandunpiyumal20@gmail.com>
2026-07-30 05:17:45 +0530

    Update KeyboardView.kt

--- app/src/main/kotlin/com/spmods/sinkey/keyboard/KeyboardView.kt
+++ app/src/main/kotlin/com/spmods/sinkey/keyboard/KeyboardView.kt
@@ -22,7 +22,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.window.Popup
