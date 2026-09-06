44378b63121887306caf1e7274559a72a4d98803
Sandun-Piumal <sandunpiyumal20@gmail.com>
2026-09-06 19:23:47 +0530

    Update ProfileScreen.kt

--- app/src/main/kotlin/com/spmods/sinkey/ui/screens/ProfileScreen.kt
+++ app/src/main/kotlin/com/spmods/sinkey/ui/screens/ProfileScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -567,9 +568,9 @@ fun ProfileScreen(
 }
 
 @Composable
-private fun IdentityStat(icon: ComposeImageVector, iconColor: Color, label: String, value: String) {
+private fun RowScope.IdentityStat(icon: ComposeImageVector, iconColor: Color, label: String, value: String) {
     Column(
-        modifier = Modifier.width(0.dp).weight(1f),
+        modifier = Modifier.weight(1f),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         androidx.compose.material3.Icon(
